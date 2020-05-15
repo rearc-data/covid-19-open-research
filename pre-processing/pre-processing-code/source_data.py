ff940dcf5904b7a756d91b297647ea6822c2d201
Derek Wolpert <me@derekwolpert.com>
2020-05-15 11:55:35 -0400

    update pre-processing, add contributing doc

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -1,24 +1,37 @@
-import os
-import csv
 import boto3
 from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 
 def source_dataset(s3_bucket, new_s3_key):
 
-    filename = 'metadata.csv'
-    source_dataset_url = 'https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/latest/metadata.csv'
+	filename = 'metadata.csv'
+	source_dataset_url = 'https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/latest/metadata.csv'
 
-    source_csv = urlopen(source_dataset_url).read().decode().splitlines()
+	# throws error occured if there was a problem accessing data
+	# otherwise downloads and uploads to s3
 
-    with open('/tmp/' + filename, 'w', encoding='utf-8') as c:
+	try:
+		response = urlopen(source_dataset_url)
 
-        c.write(source_csv[0].lower().replace(
-            ' ', '_').replace('#', '') + '\n')
+	except HTTPError as e:
+		raise Exception('HTTPError: ', e.code, filename)
 
-        for row in source_csv[1:]:
-            c.write(row + '\n')
+	except URLError as e:
+		raise Exception('URLError: ', e.reason, filename)
 
-    # uploading new s3 dataset
+	else:
 
-    s3 = boto3.client('s3')
-    s3.upload_file('/tmp/' + filename, s3_bucket, new_s3_key + filename)
+		data = response.read().decode().splitlines()
+		file_location = '/tmp/' + filename
+
+		with open(file_location, 'w', encoding='utf-8') as c:
+			c.write(data[0].lower().replace(' ', '_').replace('#', '') + '\n')
+			c.write('\n'.join(row for row in data[1:]))
+
+
+		# uploading new s3 dataset
+
+		s3 = boto3.client('s3')
+		s3.upload_file(file_location, s3_bucket, new_s3_key)
+
+		return [{'Bucket': s3_bucket, 'Key': new_s3_key}]
