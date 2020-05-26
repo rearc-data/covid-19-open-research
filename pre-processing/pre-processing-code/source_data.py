5e6d80e5d7bd0d57e9d03a845c153510df710935
Derek Wolpert <me@derekwolpert.com>
2020-05-26 10:41:51 -0400

    add retry logic to source_data.py

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -1,6 +1,8 @@
 import boto3
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
+import time
+import os
 
 def source_dataset(s3_bucket, new_s3_key):
 
@@ -10,28 +12,37 @@ def source_dataset(s3_bucket, new_s3_key):
 	# throws error occured if there was a problem accessing data
 	# otherwise downloads and uploads to s3
 
-	try:
-		response = urlopen(source_dataset_url)
+	response = None
 
-	except HTTPError as e:
-		raise Exception('HTTPError: ', e.code, filename)
+	retries = 5
+	for attempt in range(retries):
+		try:
+			response = urlopen(source_dataset_url)
+		except HTTPError as e:
+			if attempt == retries:
+				raise Exception('HTTPError: ', e.code, source_dataset_url)
+			time.sleep(0.2 * attempt)
 
-	except URLError as e:
-		raise Exception('URLError: ', e.reason, filename)
+		except URLError as e:
+			if attempt == retries:
+				raise Exception('URLError: ', e.reason, source_dataset_url)
+			time.sleep(0.2 * attempt)
+		else:
+			break
 
-	else:
+	data = response.read().decode().splitlines()
+	file_location = '/tmp/' + filename
 
-		data = response.read().decode().splitlines()
-		file_location = '/tmp/' + filename
+	with open(file_location, 'w', encoding='utf-8') as c:
+		c.write(data[0].lower().replace(' ', '_').replace('#', '') + '\n')
+		c.write('\n'.join(row for row in data[1:]))
 
-		with open(file_location, 'w', encoding='utf-8') as c:
-			c.write(data[0].lower().replace(' ', '_').replace('#', '') + '\n')
-			c.write('\n'.join(row for row in data[1:]))
+	# uploading new s3 dataset
 
+	s3 = boto3.client('s3')
+	s3.upload_file(file_location, s3_bucket, new_s3_key)
 
-		# uploading new s3 dataset
+	# deletes to preserve limited space in aws lamdba
+	os.remove(file_location)
 
-		s3 = boto3.client('s3')
-		s3.upload_file(file_location, s3_bucket, new_s3_key)
-
-		return [{'Bucket': s3_bucket, 'Key': new_s3_key}]
+	return [{'Bucket': s3_bucket, 'Key': new_s3_key}]
