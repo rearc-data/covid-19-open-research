0b8c7863060ca669d45badddb54f26ff47dff121
Derek Wolpert <48101033+derekwolpert@users.noreply.github.com>
2021-01-04 10:10:15 -0500

    csv file managed in memory

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -2,22 +2,20 @@ import boto3
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
 import time
-import os
 
 def source_dataset(s3_bucket, new_s3_key):
 
-	filename = 'metadata.csv'
 	source_dataset_url = 'https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/latest/metadata.csv'
 
 	# throws error occured if there was a problem accessing data
 	# otherwise downloads and uploads to s3
 
-	response = None
+	data = None
 
 	retries = 5
 	for attempt in range(retries):
 		try:
-			response = urlopen(source_dataset_url)
+			data = urlopen(source_dataset_url)
 		except HTTPError as e:
 			if attempt == retries:
 				raise Exception('HTTPError: ', e.code, source_dataset_url)
@@ -30,19 +28,16 @@ def source_dataset(s3_bucket, new_s3_key):
 		else:
 			break
 
-	data = response.read().decode().splitlines()
-	file_location = '/tmp/' + filename
-
-	with open(file_location, 'w', encoding='utf-8') as c:
-		c.write(data[0].lower().replace(' ', '_').replace('#', '') + '\n')
-		c.write('\n'.join(row for row in data[1:]))
+	data = data.read().decode().splitlines()
+	data[0] = data[0].lower().replace(' ', '_').replace('#', '')
+	data = '\n'.join(data)
+	data = data.encode()
 
 	# uploading new s3 dataset
-
 	s3 = boto3.client('s3')
-	s3.upload_file(file_location, s3_bucket, new_s3_key)
-
+	s3.put_object(Body=data, Bucket=s3_bucket, Key=new_s3_key)
+	
 	# deletes to preserve limited space in aws lamdba
-	os.remove(file_location)
+	data = None
 
 	return [{'Bucket': s3_bucket, 'Key': new_s3_key}]
